b963b780e975bba3c92f181192872c17d49ec7a4
Janet Joy Wilson <janetjoywilson@Janets-MacBook-Air.local>
2021-05-31 10:30:17 -0400

    flipclock

--- vendors/flipclock/timer.js
+++ vendors/flipclock/timer.js
@@ -1,7 +1,7 @@
 var today = new Date()
 
 var timer = function() {}
-timer.countdownDate = new Date("2021-04-22T12:00:00.000")
+timer.countdownDate = new Date("2022-04-22T12:00:00.000")
 
 // set date to 10 days in the future for testing purposes
 // timer.countdownDate.setDate( timer.countdownDate.getDate() + 10 );
